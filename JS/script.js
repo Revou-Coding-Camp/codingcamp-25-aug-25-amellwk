81efb6a1f608cfe39914af86aa2ab76cc7277c1b
Amellia Widyadana K <amelliawk16@gmail.com>
2025-08-29 17:31:25 +0700

    fourth
    hasil home dan profile

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,25 @@
+console.log("Hello World");
+
+window.onload = function() {
+    // greet();
+};
+
+function greet () {
+    let user = prompt("Please enter your name:");
+    document.getElementById("username").innerText = user;
+    if(user){
+        alert(`Hi ${user}, Welcome to Energi Hijau!`);
+    } else {
+        alert("Hi, Welcome to Energi Hijau!");
+    }
+}
+
 function showSidebar(){
             const sidebar = document.querySelector('.sidebar')
             sidebar.style.display = 'flex'
         }
 
-        function hideSidebar(){
+function hideSidebar(){
             const sidebar = document.querySelector('.sidebar')
             sidebar.style.display = 'none'
         }
